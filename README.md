221aa3122810a22fa0f5a443d07f17a7176feeaf
Lei Ke <keleiwhu@gmail.com>
2023-06-12 03:38:43 +0200

    update code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 > Lei Ke, Mingqiao Ye, Martin Danelljan, Yifan Liu, Yu-Wing Tai, Chi-Keung Tang, Fisher Yu \
 > ETH Zurich & HKUST 
 
-We propose HQ-SAM to upgrade SAM for high-quality zero-shot segmentation. Refer to our paper for more details. Our code and models will be released in two weeks. Stay tuned!
+We propose HQ-SAM to upgrade SAM for high-quality zero-shot segmentation. Refer to our [paper](https://arxiv.org/abs/2306.01567) for more details.
 
 Visual comparison between SAM and HQ-SAM
 -----------------
@@ -50,6 +50,79 @@ Note: Using ViT-L backbone. We adopt the SOTA model XMem as our video boxes prom
  ### Interactive segmentation comparison using various points
 Note:Using ViT-L backbone. On the high-quality COIFT (zero-shot) and DIS val set. 
 ![point_comp](figs/points_comp.png)
+
+### **Installation**
+The code requires `python>=3.8`, as well as `pytorch>=1.7` and `torchvision>=0.8`. Please follow the instructions [here](https://pytorch.org/get-started/locally/) to install both PyTorch and TorchVision dependencies. Installing both PyTorch and TorchVision with CUDA support is strongly recommended.
+
+Clone the repository locally and install with
+
+```
+git clone https://github.com/SysCV/sam-hq.git
+cd sam-hq; pip install -e .
+```
+
+The following optional dependencies are necessary for mask post-processing, saving masks in COCO format, the example notebooks, and exporting the model in ONNX format. `jupyter` is also required to run the example notebooks.
+
+```
+pip install opencv-python pycocotools matplotlib onnxruntime onnx
+```
+
+### Example conda environment setup
+```bash
+conda create --name sam_hq python=3.8 -y
+conda activate sam_hq
+conda install pytorch==1.10.0 torchvision==0.11.0 cudatoolkit=11.1 -c pytorch -c nvidia
+pip install opencv-python pycocotools matplotlib onnxruntime onnx
+
+# under your working directory
+git clone https://github.com/SysCV/sam-hq.git
+cd sam-hq
+pip install -e .
+export PYTHONPATH=$(pwd)
+```
+
+### **Model Checkpoints**
+
+Three HQ-SAM model versions of the model are available with different backbone sizes. These models can be instantiated by running
+
+```
+from segment_anything import sam_model_registry
+sam = sam_model_registry["<model_type>"](checkpoint="<path/to/checkpoint>")
+```
+
+Download the provided trained model below and put them into the pretrained_checkpoint folder:
+```
+mkdir pretrained_checkpoint
+``` 
+
+Click the links below to download the checkpoint for the corresponding model type.
+- `vit_b`: [ViT-B HQ-SAM model.](https://drive.google.com/file/d/11yExZLOve38kRZPfRx_MRxfIAKmfMY47/view?usp=sharing)
+- `vit_l`: [ViT-L HQ-SAM model.](https://drive.google.com/file/d/1Uk17tDKX1YAKas5knI4y9ZJCo0lRVL0G/view?usp=sharing)
+- `vit_h`: [ViT-H HQ-SAM model.](https://drive.google.com/file/d/1qobFYrI4eyIANfBSmYcGuWRaSIXfMOQ8/view?usp=sharing)
+
+### **Getting Started**
+
+First download a [model checkpoint](#model-checkpoints). Then the model can be used in just a few lines to get masks from a given prompt:
+
+```
+from segment_anything import SamPredictor, sam_model_registry
+sam = sam_model_registry["<model_type>"](checkpoint="<path/to/checkpoint>")
+predictor = SamPredictor(sam)
+predictor.set_image(<your_image>)
+masks, _, _ = predictor.predict(<input_prompts>)
+```
+
+Additionally, see the usage examples in our [demo](/demo/demo_hqsam.py).
+
+```
+python demo/demo_hqsam.py
+```
+
+For getting baseline SAM's visual result. Note that you need to download original SAM checkpoint from [baseline-SAM-L model](https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth) and put it into the pretrained_checkpoint folder.
+```
+python demo/demo_sam.py
+```
+
 
 Citation
 ---------------
---
+++ demo/demo_sam.py
@@ -0,0 +1,128 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+from segment_anything import sam_model_registry_baseline, SamPredictor
+import os
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+
+
+def show_res(masks, scores, input_point, input_label, input_box, filename, image):
+    for i, (mask, score) in enumerate(zip(masks, scores)):
+        plt.figure(figsize=(10,10))
+        plt.imshow(image)
+        show_mask(mask, plt.gca())
+        if input_box is not None:
+            box = input_box[i]
+            show_box(box, plt.gca())
+        if (input_point is not None) and (input_label is not None): 
+            show_points(input_point, input_label, plt.gca())
+        
+        print(f"Score: {score:.3f}")
+        plt.axis('off')
+        plt.savefig(filename+'_'+str(i)+'.png',bbox_inches='tight',pad_inches=-0.1)
+        plt.close()
+
+def show_res_multi(masks, scores, input_point, input_label, input_box, filename, image):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    for mask in masks:
+        show_mask(mask, plt.gca(), random_color=True)
+    for box in input_box:
+        show_box(box, plt.gca())
+    for score in scores:
+        print(f"Score: {score:.3f}")
+    plt.axis('off')
+    filename = result_path + 'example'+str(i)
+    plt.savefig(filename +'.png',bbox_inches='tight',pad_inches=-0.1)
+    plt.close()
+
+if __name__ == "__main__":
+    sam_checkpoint = "./pretrained_checkpoint/sam_vit_l_0b3195.pth"
+    model_type = "vit_l"
+    device = "cuda"
+    sam = sam_model_registry_baseline[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+    predictor = SamPredictor(sam)
+
+    for i in range(8):
+        print("image:   ",i)
+        image = cv2.imread('demo/input_imgs/example'+str(i)+'.png')
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        predictor.set_image(image)
+
+        if i==0:
+            input_box = np.array([[4,13,1007,1023]])
+            input_point, input_label = None, None
+        elif i==1:
+            input_box = np.array([[306, 132, 925, 893]])
+            input_point, input_label = None, None
+        elif i==2:
+            input_point = np.array([[495,518],[217,140]])
+            input_label = np.ones(input_point.shape[0])
+            input_box = None
+        elif i==3:
+            input_point = np.array([[221,482],[498,633],[750,379]])
+            input_label = np.ones(input_point.shape[0])
+            input_box = None
+        elif i==4:
+            input_box = np.array([[64,76,940,919]])
+            input_point, input_label = None, None
+        elif i==5:
+            input_point = np.array([[373,363], [452, 575]])
+            input_label = np.ones(input_point.shape[0])
+            input_box = None
+        elif i==6:
+            input_box = np.array([[181, 196, 757, 495]])
+            input_point, input_label = None, None
+        elif i==7:
+            # multi box input
+            input_box = torch.tensor([[45,260,515,470], [310,228,424,296]],device=predictor.device)
+            transformed_box = predictor.transform.apply_boxes_torch(input_box, image.shape[:2])
+            input_point, input_label = None, None
+
+        batch_box = False if input_box is None else len(input_box)>1 
+        result_path = 'demo/baseline_sam_result/'
+        os.makedirs(result_path, exist_ok=True)
+
+        if not batch_box: 
+            masks, scores, logits = predictor.predict(
+                point_coords=input_point,
+                point_labels=input_label,
+                box = input_box,
+                multimask_output=False,
+            )
+            show_res(masks,scores,input_point, input_label, input_box, result_path + 'example'+str(i), image)
+        else:
+            masks, scores, logits = predictor.predict_torch(
+                point_coords=input_point,
+                point_labels=input_label,
+                boxes=transformed_box,
+                multimask_output=False,
+            )
+            masks = masks.squeeze(1).cpu().numpy()
+            scores = scores.squeeze(1).cpu().numpy()
+            input_box = input_box.cpu().numpy()
+            show_res_multi(masks, scores, input_point, input_label, input_box, result_path + 'example'+str(i), image)
+
+
+
