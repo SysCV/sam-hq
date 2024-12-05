408500a3510fa5f3edc7d5d03b463679b5a96d4e
Mingqiao Ye <949286448@qq.com>
2024-12-05 00:32:45 +0000

    Update sam-hq2 eval seginw

---
+++ seginw/test_seginw_sam_hq2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash  
+  
+for file in ./data/seginw/*;  
+do  
+echo $file is data path \! ;  
+
+python test_ap_on_seginw_sam2.py -c GroundingDINO/groundingdino/config/GroundingDINO_SwinB.py -p pretrained_checkpoint/groundingdino_swinb_cogcoor.pth --anno_path $file/valid/_annotations_min1cat.coco.json --image_dir $file/valid/ --use_sam_hq 
+done
